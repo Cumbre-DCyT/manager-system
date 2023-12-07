9aa9390e76b79e48198acc7e2500819adeb566c1
Miguel Corbos <corbos1995@gmail.com>
2023-12-07 03:36:42 -0400

    version 1

---
+++ src/lib/components/ButtonGetAccess.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	async function handleClick() {
+		const response = await fetch('api/google', {
+			method: 'POST'
+		});
+
+		const { url } = await response.json();
+
+		goto(url);
+	}
+</script>
+
+<button on:click={handleClick}>get access</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import ButtonGetAccess from '$lib/components/ButtonGetAccess.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<ButtonGetAccess />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
